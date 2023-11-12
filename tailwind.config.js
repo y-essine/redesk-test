f4b80e847be8386dca3bea98f18bec270e922cca
YESSINE <prodtf@gmail.com>
2023-11-12 15:31:09 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#201e2d',
+        secondary: '#2a2839',
+        tertiary: '#12111B',
+        accent: '#e47373',
+        'primary-t': '#cbd5e1',
+        'secondary-t': '#A29CDE',
+        'tertiary-t': '#5a567c'
+      },
+      screens: {
+        smd: '880px',
+        sm: '640px',
+        xs: '560px',
+        '2xs': '425px'
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/line-clamp')],
+};
